09b97285c67be3c400ead89325747b86853e1400
Sonu Dutta <itsmesonudutta@gmail.com>
2022-03-17 17:44:07 +0530

    commit

--- Java/2_Strings/SubString.java
+++ Java/2_Strings/SubString.java
@@ -1,5 +1,13 @@
-// Scanner in = new Scanner(System.in);
-// String S = in.next();
-// int start = in.nextInt();
-// int end = in.nextInt();
-// System.out.println(S.substring(start,end));
+import java.util.Scanner;
+
+public class SubString{
+    public static void main(String[] args) {
+                
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start,end));
+        in.close();
+        }
+}
